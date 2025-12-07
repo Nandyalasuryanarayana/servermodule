fedcb6db68386d60cff14a436dc5aeaecabc3ded
Nandyalasuryanarayana <suryanarayana600118@gmail.com>
2025-12-07 10:18:04 +0530

    server4

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
-data "aws_ssm_parameter" "ubuntu_22" {
-  name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp3/ami-id"
+data "aws_ami" "ubuntu_22" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
 }
+
 
 resource "aws_key_pair" "public_key" {
   public_key = var.aws_key_pair
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ssm_parameter.ubuntu_22.value
+  ami           = data.aws_ami.ubuntu_22.id
   instance_type = var.instance_type
   key_name = aws_key_pair.public_key.id
   associate_public_ip_address = var.public_ip_address
